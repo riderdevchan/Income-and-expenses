2fdbe8a93cee187ecc4cf536e2207ae7bc2cea03
riderdevchan <34148113+riderdevchan@users.noreply.github.com>
2026-07-02 21:58:39 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
-const CACHE_NAME = 'money-v5';
+const CACHE_NAME = 'money-v6';
 const ASSETS = [
   'index.html',
-  'manifest.json'
+  'manifest.json',
+  'money1.png'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); // บังคับให้เริ่มทำงานทันทีโดยไม่ต้องรอแอปเก่าปิดตัว
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // ลบแคชถุงเขียวอันเก่าทิ้งให้สิ้นซาก
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
-
